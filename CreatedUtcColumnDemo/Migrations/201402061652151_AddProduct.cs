e85c5dbab6fb52544f921389f2ba6dcb82e2d115
Andy Mehalick <mehalick@gmail.com>
2014-02-06 11:53:58 -0500

    enabled migrations; added first migration

---
+++ CreatedUtcColumnDemo/Migrations/201402061652151_AddProduct.cs
@@ -0,0 +1,27 @@
+namespace CreatedUtcColumnDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProduct : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        CreatedUtc = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
