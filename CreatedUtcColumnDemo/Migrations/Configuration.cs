680864e4322dc5e67a5da0a25f7dbf3f1a33ca60
Andy Mehalick <mehalick@gmail.com>
2014-02-06 12:13:19 -0500

    added CustomSqlServerMigrationSqlGenerator

--- CreatedUtcColumnDemo/Migrations/Configuration.cs
+++ CreatedUtcColumnDemo/Migrations/Configuration.cs
@@ -1,31 +1,49 @@
+using System.Collections.Generic;
+using System.Data.Entity.Migrations.Model;
+using System.Data.Entity.SqlServer;
+using System.Data.Entity.Migrations;
+
 namespace CreatedUtcColumnDemo.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<CreatedUtcColumnDemo.DataContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("System.Data.SqlClient", new CustomSqlServerMigrationSqlGenerator());
+        }
+    }
+
+    internal class CustomSqlServerMigrationSqlGenerator : SqlServerMigrationSqlGenerator
+    {
+        protected override void Generate(AddColumnOperation addColumnOperation)
+        {
+            SetCreatedUtcColumn(addColumnOperation.Column);
+
+            base.Generate(addColumnOperation);
         }
 
-        protected override void Seed(CreatedUtcColumnDemo.DataContext context)
+        protected override void Generate(CreateTableOperation createTableOperation)
         {
-            //  This method will be called after migrating to the latest version.
+            SetCreatedUtcColumn(createTableOperation.Columns);
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            base.Generate(createTableOperation);
+        }
+
+        private static void SetCreatedUtcColumn(IEnumerable<ColumnModel> columns)
+        {
+            foreach (var columnModel in columns)
+            {
+                SetCreatedUtcColumn(columnModel);
+            }
+        }
+
+        private static void SetCreatedUtcColumn(PropertyModel column)
+        {
+            if (column.Name == "CreatedUtc")
+            {
+                column.DefaultValueSql = "GETUTCDATE()";
+            }
         }
     }
 }
