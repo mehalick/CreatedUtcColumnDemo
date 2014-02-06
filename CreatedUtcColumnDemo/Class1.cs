a4913f9d1c3e0ae5f5c0670289dad8f3327e0755
Andy Mehalick <mehalick@gmail.com>
2014-02-06 11:50:14 -0500

    added abstract EntityBase class

--- CreatedUtcColumnDemo/Class1.cs
+++ CreatedUtcColumnDemo/Class1.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 
 namespace CreatedUtcColumnDemo
 {
-    public class Class1
+    public class Product : EntityBase
     {
+        public int ProductId { get; set; }
+        public string Name { get; set; }  
+    }
+
+    public abstract class EntityBase
+    {
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedUtc { get; set; }
+    }
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
     }
 }
